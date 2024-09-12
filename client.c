7de5963115466ea21fad1b4f309d5f828619d32d
suping4 <kimjs158@gmail.com>
2024-09-12 12:21:32 +0900

    .

--- client.c
+++ client.c
@@ -25,14 +25,14 @@ struct Message {
 
 int ssock;
 int pipe_fd[2];
-pid_t child_pid;
+pid_t pid;
 struct LoginInfo login;
 
-struct Message message_history[MAX_MESSAGES];
-int message_count = 0;
+struct Message message_history[MAX_MESSAGES];  // 채팅방을 만들기 위한 히스토리를 저장할 배열
+int message_count = 0;  // 채팅 히스토리의 개수를 저장할 변수
 
 void clear_screen() {
-    printf("\033[2J\033[H");
+    printf("\033[2J\033[H");    //ANSI 이스케이프 코드를 사용하여 화면을 지우는 함수
 }
 
 void print_line() {
@@ -43,58 +43,58 @@ void print_line() {
 }
 
 void print_centered(const char* text) {
-    int padding = (SCREEN_WIDTH - strlen(text)) / 2;
-    for (int i = 0; i < padding; i++) {
-        printf(" ");
+    int padding = (SCREEN_WIDTH - strlen(text)) / 2;  // 텍스트를 가운데 정렬하기 위한 공백 계산
+    for (int i = 0; i < padding; i++) { 
+        printf(" "); 
     }
-    printf("%s\n", text);
+    printf("%s\n", text);  // 텍스트를 가운데 정렬하여 출력
 }
 
 void update_chat_screen() {
-    clear_screen();
+    clear_screen();  // 화면을 지우는 함수 호출
     print_line();
     print_centered("채팅방");
     print_centered("(종료:/q) (검색:/s)");
-    printf("    your id: %s\n", login.id);
+    printf("   your id: %s\n", login.id);
     print_line();
 
-    int start = (message_count > MAX_MESSAGES) ? message_count - MAX_MESSAGES : 0;
+    int start = (message_count > MAX_MESSAGES) ? message_count - MAX_MESSAGES : 0;  // 채팅 히스토리 출력을 위한 시작 인덱스 계산
     for (int i = start; i < message_count; i++) {
-        printf("[%s]: %s\n", message_history[i].id, message_history[i].content);
+        printf("[%s]: %s\n", message_history[i].id, message_history[i].content);  // 채팅 히스토리 출력
     }
 
     print_line();
     printf("메시지를 입력하세요 : ");
-    fflush(stdout);
+    fflush(stdout);  // 출력 버퍼를 비우는 함수 호출
 }
-
-void add_message(const char* id, const char* content) {
-    if (message_count < MAX_MESSAGES) {
-        strcpy(message_history[message_count].id, id);
-        strcpy(message_history[message_count].content, content);
-        message_count++;
-    } else {
-        for (int i = 0; i < MAX_MESSAGES - 1; i++) {
+// 메시지를 채팅 히스토리에 추가하는 함수
+void add_message(const char* id, const char* content) { 
+    if (message_count < MAX_MESSAGES) {  // 채팅 히스토리의 개수가 최대 개수보다 작을 때
+        strcpy(message_history[message_count].id, id);  // 메시지 ID를 복사
+        strcpy(message_history[message_count].content, content);  // 메시지 내용을 복사
+        message_count++;  // 메시지 개수 증가
+    } else {  // 채팅 히스토리의 개수가 최대 개수보다 클 때
+        for (int i = 0; i < MAX_MESSAGES - 1; i++) {  // 모든 메시지를 한 칸씩 앞으로 이동
             message_history[i] = message_history[i + 1];
         }
-        strcpy(message_history[MAX_MESSAGES - 1].id, id);
-        strcpy(message_history[MAX_MESSAGES - 1].content, content);
+        strcpy(message_history[MAX_MESSAGES - 1].id, id);  // 새 메시지 ID를 마지막 위치에 복사
+        strcpy(message_history[MAX_MESSAGES - 1].content, content);  // 새 메시지 내용을 마지막 위치에 복사
     }
-    update_chat_screen();
+    update_chat_screen();  // 채팅 화면 업데이트
 }
 
-void sigint_handler(int signo) {
-    close(ssock);
-    close(pipe_fd[0]);
-    close(pipe_fd[1]);
-    if (child_pid > 0) {
-        kill(child_pid, SIGTERM);
+void sigint_handler(int signo) {    // SIGINT 시그널 핸들러 함수 정의
+    close(ssock);                   // 서버 소켓 연결 종료
+    close(pipe_fd[0]);              // 파이프 읽기 종단 닫기
+    close(pipe_fd[1]);              // 파이프 쓰기 종단 닫기
+    if (pid > 0) {                  // 자식 프로세스가 존재하는 경우
+        kill(pid, SIGTERM);         // 자식 프로세스에 종료 시그널 보내기
     }
     exit(0);
 }
 
-void search_messages() {
-    char keyword[BUFSIZ];
+void search_messages() {    // 메시지를 검색하는 함수
+    char keyword[BUFSIZ];   // 검색할 키워드를 저장할 문자열 배열 선언
     printf("검색할 키워드를 입력하세요: ");
     fgets(keyword, BUFSIZ, stdin);
     keyword[strcspn(keyword, "\n")] = 0;  // 개행 문자 제거
@@ -104,15 +104,15 @@ void search_messages() {
     print_centered("검색 결과");
     print_line();
 
-    int found = 0;
+    int found = 0;  // 검색된 메시지 개수를 저장할 변수 초기화
     for (int i = 0; i < message_count; i++) {
-        if (strstr(message_history[i].content, keyword) != NULL) {
-            printf("[%s]: %s\n", message_history[i].id, message_history[i].content);
-            found++;
+        if (strstr(message_history[i].content, keyword) != NULL) {  // 메시지 내용에 키워드가 포함되어 있는지 확인
+            printf("[%s]: %s\n", message_history[i].id, message_history[i].content);  // 검색된 메시지 출력
+            found++;  // 검색된 메시지 개수 증가
         }
     }
 
-    if (found == 0) {
+    if (found == 0) {  // 검색된 메시지가 없을 때
         printf("검색 결과가 없습니다.\n");
     }
 
@@ -132,14 +132,13 @@ int main(int argc, char **argv) {
         return -1;
     }
 
-    if ((ssock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+    if ((ssock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {  // 소켓 생성
         perror("socket()");
         return -1;
     }
 
     memset(&servaddr, 0, sizeof(servaddr));
     servaddr.sin_family = AF_INET;
-    
     inet_pton(AF_INET, argv[1], &(servaddr.sin_addr.s_addr));
     servaddr.sin_port = htons(TCP_PORT);
 
@@ -148,6 +147,7 @@ int main(int argc, char **argv) {
         return -1;
     }
 
+    // 로그인 화면 출력(패스워드와 상관없이 로그인 성공)
     clear_screen();
     print_line();
     print_centered("로그인");
@@ -161,46 +161,46 @@ int main(int argc, char **argv) {
     fgets(login.password, MAX_PW_LEN, stdin);
     login.password[strcspn(login.password, "\n")] = 0;
 
-    if (send(ssock, &login, sizeof(login), 0) <= 0) {
+    if (send(ssock, &login, sizeof(login), 0) <= 0) { // 로그인 정보 서버로 전송
         perror("send()");
         return -1;
     }
 
     memset(mesg, 0, BUFSIZ);
-    if (recv(ssock, mesg, BUFSIZ, 0) <= 0) {
+    if (recv(ssock, mesg, BUFSIZ, 0) <= 0) {  // 서버로부터 로그인 결과 수신
         perror("recv()");
         return -1;
     }
 
-    if (strcmp(mesg, "로그인 성공") != 0) {
+    if (strcmp(mesg, "로그인 성공") != 0) {  // 로그인 성공이 반환이 안되면 실패 출력
         printf("로그인 실패: %s\n", mesg);
         close(ssock);
         return -1;
     }
 
-    if (pipe(pipe_fd) < 0) {
+    if (pipe(pipe_fd) < 0) {  // 파이프 생성
         perror("pipe()");
         return -1;
     }
 
-    signal(SIGINT, sigint_handler);
+    signal(SIGINT, sigint_handler);  // SIGINT 시그널 핸들러 등록
 
-    child_pid = fork();
+    pid = fork();
 
-    if (child_pid < 0) {
+    if (pid < 0) {
         perror("fork()");
         return -1;
-    } else if (child_pid == 0) {
+    } else if (pid == 0) {
         // 자식 프로세스: 메시지 수신
         close(pipe_fd[1]);  // 쓰기 파이프 닫기
         while (1) {
             struct Message received_msg;
-            memset(&received_msg, 0, sizeof(received_msg));
-            if (recv(ssock, &received_msg, sizeof(received_msg), 0) <= 0) {
+            memset(&received_msg, 0, sizeof(received_msg));  // 메시지 구조체 초기화
+            if (recv(ssock, &received_msg, sizeof(received_msg), 0) <= 0) {  // 서버로부터 메시지 수신
                 perror("recv()");
                 break;
             }
-            add_message(received_msg.id, received_msg.content);
+            add_message(received_msg.id, received_msg.content);  // 채팅 히스토리에 메시지 추가
             write(pipe_fd[0], "1", 1);  // 부모에게 메시지 수신 알림
         }
         close(pipe_fd[0]);
@@ -208,7 +208,7 @@ int main(int argc, char **argv) {
     } else {
         // 부모 프로세스: 메시지 송신
         close(pipe_fd[0]);  // 읽기 파이프 닫기
-        update_chat_screen();
+        update_chat_screen();  // 채팅 화면 업데이트
         while (1) {
             struct Message msg;
             strcpy(msg.id, login.id);
@@ -226,22 +226,22 @@ int main(int argc, char **argv) {
                 continue;
             }
 
-            add_message(msg.id, msg.content);
+            add_message(msg.id, msg.content);  // 채팅 히스토리에 메시지 추가
 
-            if (send(ssock, &msg, sizeof(msg), 0) <= 0) {
+            if (send(ssock, &msg, sizeof(msg), 0) <= 0) {  // 서버 소켓으로 메시지 전송
                 perror("send()");
                 break;
             }
 
             // 자식 프로세스의 메시지 수신 대기
             char buf[2];
-            read(pipe_fd[1], buf, 1);
+            read(pipe_fd[1], buf, 1);  // 자식 프로세스의 메시지 수신 대기
         }
         close(pipe_fd[1]);
     }
 
     close(ssock);
-    kill(child_pid, SIGTERM);
+    kill(pid, SIGTERM);
     wait(NULL);
     return 0;
 }
--- server.c
+++ server.c
@@ -8,6 +8,8 @@
 #include <fcntl.h>
 #include <signal.h>
 #include <errno.h>
+#include <syslog.h>
+#include <sys/stat.h>
 
 #define TCP_PORT 5100
 #define MAX_ID_LEN 20
@@ -24,104 +26,151 @@ struct Message {
     char content[BUFSIZ];
 };
 
-int client_sockets[MAX_CLIENTS];
-int client_count = 0;
-pid_t client_pids[MAX_CLIENTS];
+int client_sockets[MAX_CLIENTS];  // 다중 클라이언트 소켓 배열
+int client_count = 0;  // 현재 접속한 클라이언트 수
+pid_t client_pids[MAX_CLIENTS];  // 클라이언트 프로세스 ID 배열
 int pipe_fd[2]; // 파이프 디스크립터 정의
 
+// 메시지를 모든 클라이언트에게 브로드캐스트하는 함수
 void broadcast_message(struct Message *msg, int sender_sock) {
-    for (int i = 0; i < client_count; i++) {
-        if (client_sockets[i] != sender_sock) {
-            send(client_sockets[i], msg, sizeof(struct Message), 0);
+    for (int i = 0; i < client_count; i++) {    // 모든 클라이언트에 대해 반복
+        if (client_sockets[i] != sender_sock) {     // 발신자를 제외한 모든 클라이언트에게 메시지 전송
+            send(client_sockets[i], msg, sizeof(struct Message), 0);   // 클라이언트 소켓으로 메시지 전송
         }
     }
 }
 
 void remove_client(int csock) {
-    for (int i = 0; i < client_count; i++) {
-        if (client_sockets[i] == csock) {
-            // 마지막 클라이언트를 현재 위치로 이동
-            client_sockets[i] = client_sockets[client_count - 1];
-            client_pids[i] = client_pids[client_count - 1];
-            client_count--;
+    for (int i = 0; i < client_count; i++) {    // 모든 클라이언트에 대해 반복
+        if (client_sockets[i] == csock) {    // 제거할 클라이언트 소켓을 찾았을 때
+            client_sockets[i] = client_sockets[client_count - 1];    // 마지막 클라이언트의 소켓을 현재 위치로 이동
+            client_pids[i] = client_pids[client_count - 1];    // 마지막 클라이언트의 프로세스 ID를 현재 위치로 이동
+            client_count--;    // 클라이언트 수 감소
             printf("클라이언트 제거됨. 현재 접속자 수: %d\n", client_count);
             break;
         }
     }
 }
 
+// 자식 프로세스 종료를 처리하는 시그널 핸들러
 void sigchld_handler(int signo) {
     pid_t pid;
-    int status;
-    while ((pid = waitpid(-1, &status, WNOHANG)) > 0) {
-        for (int i = 0; i < client_count; i++) {
-            if (client_pids[i] == pid) {
-                remove_client(client_sockets[i]);
+    int status;  // 자식 프로세스 상태 변수
+    while ((pid = waitpid(-1, &status, WNOHANG)) > 0) {    // 자식 프로세스가 종료되었을 때
+        for (int i = 0; i < client_count; i++) {    // 모든 클라이언트에 대해 반복
+            if (client_pids[i] == pid) {    // 종료된 자식 프로세스를 찾았을 때
+                remove_client(client_sockets[i]);    // 클라이언트 제거
                 break;
             }
         }
     }
 }
 
-void sigusr1_handler(int signo) {
+void sigusr1_handler(int signo) { // 자식 프로세스가 메시지를 보내면 부모 프로세스에게 알리는 시그널 핸들러
     struct Message msg;
-    read(pipe_fd[0], &msg, sizeof(msg));
-    broadcast_message(&msg, -1);
+    read(pipe_fd[0], &msg, sizeof(msg));    // 파이프에서 메시지 읽기
+    broadcast_message(&msg, -1);    // 모든 클라이언트에게 메시지 브로드캐스트
+}
+
+void daemonize() {
+    pid_t pid, sid;
+
+    // 부모 프로세스 종료
+    pid = fork();
+    if (pid < 0) {
+        exit(EXIT_FAILURE);
+    }
+    if (pid > 0) {
+        exit(EXIT_SUCCESS);
+    }
+
+    // 새로운 세션 생성
+    sid = setsid();
+    if (sid < 0) {
+        exit(EXIT_FAILURE);
+    }
+
+    // 작업 디렉토리 변경
+    if ((chdir("/")) < 0) {
+        exit(EXIT_FAILURE);
+    }
+
+    // 파일 권한 마스크 재설정
+    umask(0);
+
+    // 표준 파일 디스크립터를 /dev/null로 리다이렉션
+    int null_fd = open("/dev/null", O_RDWR);
+    if (null_fd != -1) {
+        dup2(null_fd, STDIN_FILENO);
+        dup2(null_fd, STDOUT_FILENO);
+        dup2(null_fd, STDERR_FILENO);
+        if (null_fd > 2) {
+            close(null_fd);
+        }
+    }
+
+    // syslog 열기
+    openlog("chat_server", LOG_PID | LOG_NDELAY, LOG_DAEMON);
 }
 
 int main(int argc, char **argv)
 {
-    int ssock; /* 소켓 디스크립트 정의 */
-    socklen_t clen;
-    pid_t pid;
+    // 데몬화 과정 추가
+    daemonize();
+
+    // syslog를 사용하여 로그 남기기
+    syslog(LOG_NOTICE, "채팅 서버 데몬이 시작되었습니다.");
+
+    int ssock; // 서버 소켓 디스크립터  
+    socklen_t clen; // 클라이언트 주소 길이
+    pid_t pid; // 자식 프로세스 ID
     int n;  // 데이터 전송 및 수신 변수
-    struct sockaddr_in servaddr, cliaddr; /* 주소 구조체 정의 */
+    struct sockaddr_in servaddr, cliaddr; // 서버 및 클라이언트 주소 구조체
     char mesg[BUFSIZ];
 
-    signal(SIGCHLD, sigchld_handler);
-    signal(SIGUSR1, sigusr1_handler);
+    signal(SIGCHLD, sigchld_handler);   // 자식 프로세스 종료 시그널 핸들러
+    signal(SIGUSR1, sigusr1_handler);   // 자식 프로세스가 메시지를 보내면 부모 프로세스에게 알리는 시그널 핸들러
 
-    /* 서버 소켓 생성 */
-    if ((ssock = socket(AF_INET, SOCK_STREAM, 0)) < 0){
+    if ((ssock = socket(AF_INET, SOCK_STREAM, 0)) < 0){    // 서버 소켓 생성
         perror("socket()");
         return -1;
     }
 
-    /* 파이프 생성 */
-    if (pipe(pipe_fd) < 0) {
+    if (pipe(pipe_fd) < 0) {    // 파이프 생성
         perror("pipe()");
         return -1;
     }
 
-    /* 주소 구조체에 주소 지정 */
+    // 주소 구조체에 주소 지정
     memset(&servaddr, 0, sizeof(servaddr));
-    servaddr.sin_family = AF_INET;
-    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-    servaddr.sin_port = htons(TCP_PORT); /* 사용할 포트 지정 */
+    servaddr.sin_family = AF_INET;  // 주소 체계 설정
+    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);  // 서버 IP 주소 설정
+    servaddr.sin_port = htons(TCP_PORT); // 서버 포트 설정
 
-    /* bind 함수를 사용하여 서버 소켓의 주소 설정 */
+    // bind 함수를 사용하여 서버 소켓의 주소 설정
     if (bind(ssock, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0){
         perror("bind()");
         return -1;
     }
 
-    /* 동시에 접속하는 클라이언트의 처리를 위한 대기 큐를 설정 */
+    // 동시에 접속하는 클라이언트의 처리를 위한 대기 큐를 설정(최대 8개 접속)
     if (listen(ssock, 8) < 0){
         perror("listen()");
         return -1;
     }
 
-    printf("서버가 시작되었습니다. 포트 %d\n", TCP_PORT);
+    // printf 대신 syslog 사용
+    syslog(LOG_NOTICE, "서버가 시작되었습니다. 포트 %d", TCP_PORT);
 
     while (1) {
-        clen = sizeof(cliaddr);
-        int csock = accept(ssock, (struct sockaddr *)&cliaddr, &clen);
+        clen = sizeof(cliaddr);  // 클라이언트 주소 길이 초기화
+        int csock = accept(ssock, (struct sockaddr *)&cliaddr, &clen);  // 클라이언트 연결 accept
         if (csock < 0) {
             perror("accept()");
             continue;
         }
 
-        if (client_count >= MAX_CLIENTS) {
+        if (client_count >= MAX_CLIENTS) {   // 접속 클라이언트 수가 최대 클라이언트 수에 도달했을 때
             printf("최대 클라이언트 수에 도달했습니다. 연결을 거부합니다.\n");
             close(csock);
             continue;
@@ -129,9 +178,8 @@ int main(int argc, char **argv)
 
         if ((pid = fork()) < 0) {
             perror("fork()");
-        } else if (pid == 0) {
-            // 자식 프로세스
-            close(ssock);
+        } else if (pid == 0) {    // 자식 프로세스
+            close(ssock);    // 서버 소켓 닫기
             close(pipe_fd[0]); // 파이프 읽기 닫기
 
             // 로그인 정보 수신
@@ -141,7 +189,7 @@ int main(int argc, char **argv)
                 exit(1);
             }
 
-            // 무조건 로그인 성공
+            // 패스워드와 관계 없이 무조건 로그인 성공
             strcpy(mesg, "로그인 성공");
 
             // 인증 결과 전송
@@ -150,46 +198,51 @@ int main(int argc, char **argv)
                 exit(1);
             }
 
-            printf("사용자 '%s' 로그인 성공\n", login.id);
+            // printf 대신 syslog 사용
+            syslog(LOG_NOTICE, "사용자 '%s' 로그인 성공", login.id);
 
             while (1) {
                 struct Message msg;
                 memset(&msg, 0, sizeof(msg));
                 // 클라이언트로부터 메시지 읽기
-                n = recv(csock, &msg, sizeof(msg), 0);
+                n = recv(csock, &msg, sizeof(msg), 0);  // 클라이언트로부터 메시지 수신
                 if (n <= 0) {
-                    if (n < 0 && errno == EINTR) continue;
+                    if (n < 0 && errno == EINTR) continue;    // 수신 실패 시 다시 시도
                     perror("클라이언트로부터 recv() 실패");
                     break;
                 }
-                printf("클라이언트로부터 받은 메시지: %s: %s\n", msg.id, msg.content);
+                // printf 대신 syslog 사용
+                syslog(LOG_NOTICE, "클라이언트로부터 받은 메시지: %s: %s", msg.id, msg.content);
 
                 // 파이프를 통해 부모 프로세스에게 메시지 전달
-                write(pipe_fd[1], &msg, sizeof(msg));
-                kill(getppid(), SIGUSR1);
+                write(pipe_fd[1], &msg, sizeof(msg));  // 파이프에 메시지 쓰기
+                kill(getppid(), SIGUSR1);  // getppid()를 사용하여 부모 프로세스에게 시그널 전달
 
                 if (strcmp(msg.content, "q") == 0) {
-                    printf("클라이언트 %s 종료\n", msg.id);
+                    // printf 대신 syslog 사용
+                    syslog(LOG_NOTICE, "클라이언트 %s 종료", msg.id);
                     break;
                 }
-            }
+            } 
 
             close(pipe_fd[1]);
             close(csock);
             exit(0);
         } else {
             // 부모 프로세스
-            client_sockets[client_count] = csock;
-            client_pids[client_count] = pid;
-            client_count++;
+            client_sockets[client_count] = csock;  // 클라이언트 소켓을 배열에 저장
+            client_pids[client_count] = pid;  // 클라이언트 프로세스 ID를 배열에 저장
+            client_count++;  // 클라이언트 수 증가
             
-            inet_ntop(AF_INET, &cliaddr.sin_addr, mesg, BUFSIZ);
-            printf("클라이언트 연결됨: %s\n", mesg);
+            inet_ntop(AF_INET, &cliaddr.sin_addr, mesg, BUFSIZ);  // 클라이언트 IP 주소를 문자열로 변환
+            // printf 대신 syslog 사용
+            syslog(LOG_NOTICE, "클라이언트 연결됨: %s", mesg);  // 연결된 클라이언트의 IP 주소 출력
         }
     }
 
     close(pipe_fd[0]);
     close(pipe_fd[1]);
     close(ssock);
+    closelog();
     return 0;
 }
